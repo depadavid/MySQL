64740f09e4861e4ac7d97ebd6237c72c02b66d6d
depadavid <100303655+depadavid@users.noreply.github.com>
2024-07-08 14:26:04 -0500

    feat: :tada: Logistica proyecto
    Indicaciones proyecto logistica

---
+++ connect.js
@@ -0,0 +1,18 @@
+import {MongoClient} from "mongodb";
+export class connect {
+    user;
+    port;
+    #host;
+    #cluster;
+    #dbName;
+    static instance;
+    constructor (user:u, port:p, pass:w, host: h, cluster:c, dbName: d) ={
+        user: "mongo",
+        port: 47797,
+        pass:
+        host: "mongodb://",
+        
+
+    
+
+}
